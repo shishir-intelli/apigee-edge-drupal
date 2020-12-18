26900a22d368e7ae30818464c659f85f9d57675b
shishir <shishir.suvarna@intelliswift.com>
2020-12-18 13:40:30 +0530

    Removed field_ui test - testing errors

--- tests/src/Functional/DeveloperAppFieldTest.php
+++ tests/src/Functional/DeveloperAppFieldTest.php
@@ -26,7 +26,7 @@ use Drupal\apigee_edge\Entity\DeveloperApp;
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Entity\EntityStorageInterface;
 use Drupal\Core\Url;
-use Drupal\Tests\field_ui\Traits\FieldUiTestTrait;
+//use Drupal\Tests\field_ui\Traits\FieldUiTestTrait;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
@@ -38,7 +38,7 @@ use Symfony\Component\HttpFoundation\Response;
  */
 class DeveloperAppFieldTest extends ApigeeEdgeFunctionalTestBase {
 
-  use FieldUiTestTrait;
+  //use FieldUiTestTrait;
 
   /**
    * {@inheritdoc}
